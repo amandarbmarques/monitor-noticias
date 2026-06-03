00fff7aaf94bbeba0ba7703f1ec5e6fdd5859e42
amandarbmarques <30411929+amandarbmarques@users.noreply.github.com>
2026-06-03 19:28:42 -0300

    Update folha.py

--- folha.py
+++ folha.py
@@ -1,35 +1,33 @@
 import feedparser
 from datetime import datetime
-
 from author import get_author
 from database import insert_news
 
-
 def coletar_folha():
-
     rss = feedparser.parse(
         "https://feeds.folha.uol.com.br/poder/rss091.xml"
     )
 
     for item in rss.entries:
-
         autor = get_author(item.link)
+
+        # Trata e padroniza a data do RSS
+        if hasattr(item, "published_parsed") and item.published_parsed:
+            data_iso = datetime(*item.published_parsed[:6]).isoformat()
+        else:
+            data_iso = datetime.now().isoformat()
 
         noticia = {
             "veiculo": "Folha",
             "titulo": item.title,
             "autor": autor,
             "url": item.link,
-            "data_publicacao": item.published,
+            "data_publicacao": data_iso,
             "data_coleta": datetime.now().isoformat()
         }
 
         insert_news(noticia)
-
-        print(
-            f"Salvo: {item.title} | {autor}"
-        )
-
+        print(f"Salvo Folha: {item.title} | {autor}")
 
 if __name__ == "__main__":
     coletar_folha()
