2cd227c878a24a1d386397e931219f0a62f22687
amandarbmarques <30411929+amandarbmarques@users.noreply.github.com>
2026-06-05 16:17:12 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 from database import create_table
-# Importa o funcionário da Folha (se o seu arquivo tiver outro nome, ajuste aqui)
-from folha import sua_funcao_da_folha  
-# Importa o NOSSO NOVO funcionário do Caminhão
+from folha import coletar_folha
 from uol import coletar_via_google_news
 
 def iniciar_coleta():
     print("🚀 Iniciando o robô de coleta...")
     
-    # 1. Garante que o banco está criado
+    # 1. Cria a tabela no Supabase (se não existir)
     create_table()
     
     # 2. Manda a Folha trabalhar
     try:
         print("📰 Coletando Folha...")
-        sua_funcao_da_folha() # <- Mude para o nome real da sua função da folha
+        coletar_folha()
     except Exception as e:
         print(f"❌ Erro na Folha: {e}")
         
-    # 3. MANDA O CAMINHÃO DO HUB TRABALHAR (UOL, GLOBO, ESTADÃO, ETC)
+    # 3. Manda o Caminhão do Hub trabalhar (UOL, G1, Estadão, etc)
     try:
         print("🚛 Iniciando o Hub do Google News...")
         coletar_via_google_news()
